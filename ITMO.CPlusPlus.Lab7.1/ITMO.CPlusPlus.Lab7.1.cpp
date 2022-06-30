daeb4d483b359dc583d68fe3f0cbcef8d59210d4
Polina-Oleshkevich <94075525+Polina-Oleshkevich@users.noreply.github.com>
2022-06-30 16:03:47 +0300

    Add files via upload

---
+++ ITMO.CPlusPlus.Lab7.1/ITMO.CPlusPlus.Lab7.1.cpp
@@ -0,0 +1,73 @@
+// ITMO.CPlusPlus.Lab7.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+#include<conio.h>
+using namespace std;  
+struct Distance
+{
+    int feet;
+    double inches;
+    void ShowDist(Distance d)
+    {
+        cout << d.feet << "\'-" << d.inches << "\"\n";
+    }
+};
+Distance AddDist(const Distance& d1, const Distance& d2)
+{ 
+    Distance d;
+    d.feet = d1.feet + d2.feet; 
+    d.inches = d1.inches + d2.inches; 
+if (d.inches >= 12.0)
+{
+    d.inches -= 12.0;  
+    d.feet++; 
+} 
+return d;
+}
+Distance InputDist()
+{
+    Distance d;
+    cout << "\n¬ведите число футов: ";  
+cin >> d.feet;
+cout << "¬ведите число дюймов: ";
+cin >> d.inches;
+return d; 
+}
+
+int main()
+{
+   
+    Distance d1 = InputDist();
+    Distance d2 = { 1, 6.25 }; 
+    Distance d3 = AddDist(d1, d2);
+    d1.ShowDist(d1);
+    d2.ShowDist(d2);
+    d3.ShowDist(d3);
+    int n;
+    cout << "¬ведите размер массива рассто€ний ";
+    cin >> n;
+    Distance* masDist = new Distance[n];
+    for (int i = 0; i < n; i++)
+    {
+        masDist[i] = InputDist(); 
+    }
+    for (int i = 0; i < n; i++)
+    { 
+        ShowDist(masDist[i]);
+    }
+    delete[] masDist;
+        return 0; 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ ITMO.CPlusPlus.Lab7.4/ITMO.CPlusPlus.Lab7.4.cpp
@@ -0,0 +1,45 @@
+// ITMO.CPlusPlus.Test7.4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream> 
+#include <windows.h> 
+#include <string>
+#include <tuple> 
+#include <vector> 
+
+using namespace std; 
+
+using Tuple = tuple<string, int, double>;
+void printTupleOfThree(Tuple t)
+{ 
+    cout << "(" << std::get<0>(t) << ", " << std::get<1>(t) << ", " << std::get<2>(t) << ")" << endl;
+}
+Tuple funtup(string s, int a, double d) 
+{
+    s.append("!"); 
+    return make_tuple(s, a, d * 10); 
+}
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    vector<string> v1{ "one", "two", "three", "four", "five", "six" }; 
+    vector<int> v2 = { 1, 2, 3, 4, 5, 6 };  
+    vector<double> v3 = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+    auto t0 = make_tuple(v1[0], v2[0], v3[0]);
+    printTupleOfThree(t0);
+    auto t1 = funtup(v1[1], v2[1], v3[1]);
+    printTupleOfThree(t1);
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
